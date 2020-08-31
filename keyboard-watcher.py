cd1b420f19f0c2d948b477472d53e56a3a7895d9
Andy Jack <github@veracity.ca>
2020-08-31 10:50:05 -0400

    Fix some whitespace issues & grammar in comments

--- keyboard-watcher.py
+++ keyboard-watcher.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
 from ctypes import cdll, c_uint
-from pyudev import Context, Monitor, MonitorObserver
+from pyudev import Context, Monitor
 from re import match
 from subprocess import run
 from sys import argv, exit
 from time import sleep
+
 
 def usage():
     print("""
@@ -13,6 +14,7 @@ Commands:
     monitor - watch udev for usb input device insertions, and remap
     remap   - run all the remap commands once and exit
 """)
+
 
 def monitor():
     context = Context()
@@ -24,16 +26,17 @@ def monitor():
         # Inserting the yubikey generates several 'add' events, but we don't
         # want to remap on every one of them.  So filter out a single one, wait
         # a bit for things to settle, and remap.
-        if device.action == 'add' and match(r'.*input\d+$',device.device_path):
+        if device.action == 'add' and match(r'.*input\d+$', device.device_path):
             sleep(2)
             remap()
+
 
 def remap():
     # compose:ralt - right alt is the compose key, e.g. for accented character entry
     run(args='setxkbmap -option compose:ralt', shell=True, check=True)
 
     # This X11 stuff is to turn off the caps lock *effect*. If it was turned
-    # on, and you've somehow no longer have a way to send the CAPS LOCK keycode,
+    # on, and you somehow no longer have a way to send the CAPS LOCK keycode,
     # there's no way to turn off the effect with the keyboard.
     #
     # https://askubuntu.com/a/80301
@@ -42,6 +45,7 @@ def remap():
     # '2' corresponds to Caps Lock
     X11.XkbLockModifiers(display, c_uint(0x0100), c_uint(2), c_uint(0))
     X11.XCloseDisplay(display)
+
 
 if len(argv) < 2:
     usage()
@@ -54,4 +58,3 @@ elif argv[1] == 'remap':
 else:
     usage()
     exit(1)
-
